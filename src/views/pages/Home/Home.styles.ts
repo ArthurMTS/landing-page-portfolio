3fe29a02ad1cf0e88788e45f46408058eb0efe36
Arthur Silva <arthurmatsilva@outlook.com>
2023-01-11 09:47:03 -0300

    feat: custom scrollbar

--- src/views/pages/Home/Home.styles.ts
+++ src/views/pages/Home/Home.styles.ts
@@ -2,7 +2,7 @@ import { styled } from "@mui/system";
 import { colors } from "assets/pallet/colors";
 
 export const ViewPage = styled("section")({
-  width: "98.79vw",
+  width: "99.3vw",
   height: "100vh",
   backgroundColor: colors.bgPrimary,
   "&.last": {
